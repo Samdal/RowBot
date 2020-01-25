1390e822ac8748e1547b9e42266fe9397eb1143b
Vemund Wannag <voldenissen@gmail.com>
2020-01-25 16:44:03 +0100

    Update help.py

--- commands/help.py
+++ commands/help.py
@@ -1,9 +1,10 @@
 from discord.ext import commands
+from discord import Embed
 
 
 @commands.command()
 async def help(ctx): # hjelpemeldingen
-    embed = Embed(title="Here you go", description="""
+    embed = Embed(title="Commands", description="""
 :small_blue_diamond: +gilf sends a sexy picture ( ͡° ͜ʖ ͡°) of (hopefully) Otto
 :small_blue_diamond: +emphasise makes text ***l   o   n   g***
 :small_blue_diamond: +mock makes the text lIKe THis
