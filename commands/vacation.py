4243e102ab6ecc3b42343b8d33305d1a5a79d7c5
voiiden <31285047+voiiden@users.noreply.github.com>
2020-04-03 16:40:59 +0200

    Update vacation.py
    Changed to summer break.

--- commands/vacation.py
+++ commands/vacation.py
@@ -3,7 +3,7 @@ import datetime
 
 @commands.command(description="Prints the amount of days until the next school break")
 async def vacation(ctx):
-    time_difference = datetime.datetime(2020, 2, 22) - datetime.datetime.now()
+    time_difference = datetime.datetime(2020, 6, 19) - datetime.datetime.now()
     await ctx.send(f'Days until winter break: {time_difference.days}')
 
 def setup(bot):
